71e04fc1d295968cac018c4de6e3e0a1730522d8
ChitwanRana <kushr4446@gmail.com>
2024-12-16 22:43:16 +0530

    Update Database

--- DocumentManager/DocumentManager/settings.py
+++ DocumentManager/DocumentManager/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DocumentManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'documentmanager',     
+        'USER': 'postgres',          
+        'PASSWORD': '204646',       
+        'HOST': 'localhost',               
+        'PORT': '5432',                    
     }
 }
 
