3921ccc627b9024983239b2a17760ecd3ad7942f
ChitwanRana <kushr4446@gmail.com>
2024-12-27 12:20:09 +0530

    Update Dashboard

--- DocumentManager/Document/forms.py
+++ DocumentManager/Document/forms.py
@@ -1,14 +1,19 @@
 from django import forms
 from django.contrib.auth.models import User
 
-
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    confirm_password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ['username', 'password', 'first_name', 'last_name', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control'}),
+        }
 
     def clean(self):
         cleaned_data = super().clean()
@@ -18,6 +23,7 @@ class RegistrationForm(forms.ModelForm):
         if password != confirm_password:
             raise forms.ValidationError("Passwords do not match.")
         return cleaned_data
+
 
 
 class LoginForm(forms.Form):
