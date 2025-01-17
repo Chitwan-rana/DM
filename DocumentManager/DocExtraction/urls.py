02c4bb97a8b86b34209ac54476ae298220925410
ChitwanRana <kushr4446@gmail.com>
2025-01-17 19:57:58 +0530

    Update Folder Structure

---
+++ DocumentManager/DocExtraction/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from .views import view_name
+
+urlpatterns = [
+#     path('editor/', view_name, name='DocEditor'),
+    # path('download/<str:file_name>/', download_file, name='download_file'),
+    # path('editor/<str:original_file_name>/', download_all, name='editor_download_all'),
+
+]
