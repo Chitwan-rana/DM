62d3b159dc11a4e01d7e28e52c8edf01131fee09
ChitwanRana <kushr4446@gmail.com>
2025-08-24 15:26:52 +0530

    Docker Push Success

--- DocumentManager/DocumentManager/settings.py
+++ DocumentManager/DocumentManager/settings.py
@@ -143,6 +143,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR /'static')]
+# Folder where collectstatic will place all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
