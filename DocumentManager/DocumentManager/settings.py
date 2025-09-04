c61c44aad9dce97f8214a83b293c23943c88ac0e
ChitwanRana <kushr4446@gmail.com>
2025-09-04 12:12:08 +0530

    Add static file

--- DocumentManager/DocumentManager/settings.py
+++ DocumentManager/DocumentManager/settings.py
@@ -142,11 +142,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR /'static')]
-# Folder where collectstatic will place all static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_volume')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "DocumentManager", "static", "assets"),
+]
 
 
 MEDIA_URL = '/media/'
